be21736f835dc2a7af2024fc9dccb6f64d81e352
Murillo Welsi <murillo.welsi@gmail.com>
2021-06-24 17:51:18 +0100

    added first routes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', function (req, res) {
-	res.send({ message: 'welcome to express' });
-});
-
-app.post('/courses', (req, res) => {
-	const { name } = req.body;
-	return res.json({ name });
-});
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => console.log(`Server is running!!`));
